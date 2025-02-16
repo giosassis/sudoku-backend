d12813c6febd6fc565e7e6dfdf36fd61b393e7a7
Giovana Assis <giovana.sant@hotmail.com>
2025-02-16 12:37:07 -0300

    adding cloudfront validation

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
-from api.game import router as game_router 
+from api.game import router as game_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Sudoku API!"}
 
+
 app.include_router(game_router, prefix="/game", tags=["game"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["http://localhost:5173"],
+    allow_origin_regex=r"https:\/\/.*\.cloudfront\.net",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
